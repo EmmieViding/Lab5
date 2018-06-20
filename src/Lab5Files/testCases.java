6bb5e74eacc72efb281cef851807b7bea551b757
lab5\emmie <emmieviding@hotmail.se>
2018-06-20 22:55:35 +0200

    Added test case for user story 2

--- src/Lab5Files/testCases.java
+++ src/Lab5Files/testCases.java
@@ -12,9 +12,19 @@ public class testCases {
 	public void userStory1() {
 		String result;
 
-		result = sc.userStory1(2, 3);
+		result = sc.userStory1(2,3);
 		
 		assertEquals("[2, 3]", result);
+	}
+	
+	@Test
+	public void userStory2() {
+		int result;
+		int firstThrow = 5;
+		int secondThrow = 3;
+		
+		result = firstThrow + secondThrow;
+		assertEquals(8, result);
 	}
 	
 
