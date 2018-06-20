983a0651ba67ec2d932170e9eb9ab376da4d7b6a
lab5\emmie <emmieviding@hotmail.se>
2018-06-20 22:52:39 +0200

    implemented the source code (in sourceCode.java file) for user story 1. I also rerunned the test case for user story 1, where I used an sourceCode object to test the implemented source code

--- src/Lab5Files/sourceCode.java
+++ src/Lab5Files/sourceCode.java
@@ -2,5 +2,19 @@ package Lab5Files;
 
 public class sourceCode {
 
-
+	public String userStory1(int firstThrow, int secondThrow)
+	{
+		String result = null;
+		
+		if(firstThrow > 10 || secondThrow > 10 || firstThrow > 10 && secondThrow > 10 )
+		{
+			result = "Error";
+		}
+		else {
+			result = ("[" + firstThrow + ", " + secondThrow + "]");
+		}
+		
+		return result;
+	}
+	
 }
--- src/Lab5Files/testCases.java
+++ src/Lab5Files/testCases.java
@@ -5,24 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class testCases {
+	
+	sourceCode sc = new sourceCode();
 
 	@Test
 	public void userStory1() {
-		int firstThrow;
-		int secondThrow;
 		String result;
-		
-		firstThrow = 2;
-		secondThrow = 3;
-		
-		result = ("[" + firstThrow + ", " + secondThrow + "]");
+
+		result = sc.userStory1(2, 3);
 		
 		assertEquals("[2, 3]", result);
 	}
 	
-	@Test
-	public void userStory2() {
-		
-	}
 
 }
