f219b03f9252840b5afb6355a5a4f0119733c287
lab5\emmie <emmieviding@hotmail.se>
2018-06-20 21:19:06 +0200

    Added a .java file to implement the testcases, added also another .java file to use later for the source code

---
+++ src/Lab5Files/sourceCode.java
@@ -0,0 +1,5 @@
+package Lab5Files;
+
+public class sourceCode {
+
+}
---
+++ src/Lab5Files/testCases.java
@@ -0,0 +1,14 @@
+package Lab5Files;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCases {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
