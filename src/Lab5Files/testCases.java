3121ba57992671627a1a5f3f32ed9ba7ebd4ccb1
lab5\emmie <emmieviding@hotmail.se>
2018-06-21 10:51:24 +0200

    implemented test case for user story 4 and it all worked as it should

--- src/Lab5Files/testCases.java
+++ src/Lab5Files/testCases.java
@@ -36,5 +36,18 @@ public class testCases {
 		assertEquals("[0,1][2,3][4,5][6,2][0,10][1,9][5,5][0,0][2,8][0,1]", result);
 	}
 	
+	@Test
+	public void userStory4() {
+		int[] gameArray = {0, 1, 2, 3, 4, 5, 6, 2, 0, 10, 1, 9, 5, 5, 0, 0, 2, 8, 0, 1};
+		int result = 0;
+		
+		for(int i = 0; i < gameArray.length; i++)
+		{
+			result += gameArray[i];
+		}
+		
+		assertEquals(64, result);
+	}
+	
 
 }
