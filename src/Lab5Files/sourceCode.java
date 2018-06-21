f3b6b72705ab596a006a12406171e5adc53663a0
lab5\emmie <emmieviding@hotmail.se>
2018-06-21 22:31:38 +0200

    After I had done the first user story I wrote code in my sourceCode.java file which I later used in my test case for user story 1. I have now changed the test case for user story 1 and are not using code from the sourceCode.java file anymore

--- src/Lab5Files/sourceCode.java
+++ src/Lab5Files/sourceCode.java
@@ -2,20 +2,6 @@ package Lab5Files;
 
 public class sourceCode {
 
-	public String userStory1(int firstThrow, int secondThrow)
-	{
-		String result = null;
-		
-		if(firstThrow > 10 || secondThrow > 10 || firstThrow > 10 && secondThrow > 10 )
-		{
-			result = "Error";
-		}
-		else {
-			result = ("[" + firstThrow + ", " + secondThrow + "]");
-		}
-		
-		return result;
-	}
 	
 	public int userStory2(int firstThrow, int secondThrow)
 	{
